d88808c6c42db7825409f565f2a657b0560d8007
Shawn Canavan <shawncanavan@Shawns-MacBook-Pro-2.local>
2023-02-11 11:31:04 -0500

    more light work, trying to figure out collision

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,7 @@ const c = canvas.getContext('2d');
     TODO: move character to different .js file and import it instead
     TODO: add blocking 
     TODO: game over screen
-    TODO: character collision, to prevent them from going behind each other
+    TODO: character collision, to prevent them from going inside of each other
 */
 
 canvas.height = 576;
@@ -69,7 +69,8 @@ class Character {
   //TODO figure out if the attack rectangle collides/overlaps with the enemy character
   attack() {
     //*air attack, which is a downwards strike, if character is NOT on the floor. otherwise regular strike
-    if (char1.y < 426) {
+    //*will need to change this since I reference char2 in a class. maybe the attack box is made outside character class?
+    if (char1.y < 426 && char2.x > this.x + this.width) {
       c.fillStyle = 'pink';
       c.fillRect(
         this.x + this.width,
@@ -77,7 +78,7 @@ class Character {
         this.width / 4,
         this.height / 1.3
       );
-    } else {
+    } else if (char1.y > 426 && char2.x > this.x + this.width) {
       char1.velocity.x = 0;
       c.fillStyle = 'pink';
       c.fillRect(
@@ -86,13 +87,31 @@ class Character {
         this.width,
         this.height / 3
       );
+    } else if (char1.y < 426 && char2.x < this.x + this.width) {
+      c.fillStyle = 'pink';
+      c.fillRect(
+        this.x,
+        this.y + this.height / 1.5,
+        this.width / -4,
+        this.height / 1.3
+      );
+    } else if (char1.y > 426 && char2.x > this.x + this.width) {
+      char1.velocity.x = 0;
+      c.fillStyle = 'pink';
+      c.fillRect(
+        this.x,
+        this.y + this.height / 2.5,
+        this.width,
+        this.height / 3
+      );
+      c.fillRect();
     }
   }
 }
 
 const char1 = new Character({
-  x: 0,
-  y: 0,
+  x: 160,
+  y: 426,
   color: 'blue',
   width: 100,
   height: 150,
@@ -104,8 +123,8 @@ const char1 = new Character({
 });
 
 const char2 = new Character({
-  x: 600,
-  y: 0,
+  x: 807,
+  y: 426,
   color: 'green',
   width: 100,
   height: 150,
@@ -175,6 +194,24 @@ const keyPressed = {
   },
 };
 
+function detectCollision(char1, char2) {
+  let collision = false;
+
+  if (
+    char1.x < char2.x + char2.width &&
+    char1.x + char1.width > char2.x &&
+    char1.y < char2.y + char2.height &&
+    char1.height + char1.y > char2.y
+  ) {
+    collision = true;
+  }
+
+  if (collision) {
+    char1.velocity.x = 0;
+  }
+  return collision;
+}
+
 /*
  *recursive function that updates the game state
  *starts by clearing the canvas, then redraws the characters
@@ -186,15 +223,18 @@ const keyPressed = {
 function updateGameStatus() {
   window.requestAnimationFrame(updateGameStatus);
   c.clearRect(0, 0, canvas.width, canvas.height);
+  let isCollision = detectCollision(char1, char2);
   char1.update();
   char2.update();
 
-  if (keyPressed.a.pressed) {
-    char1.velocity.x = -1;
-  } else if (keyPressed.d.pressed) {
-    char1.velocity.x = 2;
-  } else {
-    char1.velocity.x = 0;
+  if (!isCollision) {
+    if (keyPressed.a.pressed) {
+      char1.velocity.x = -1;
+    } else if (keyPressed.d.pressed) {
+      char1.velocity.x = 2;
+    } else {
+      char1.velocity.x = 0;
+    }
   }
 
   //TODO, run collision detection to check for damage
